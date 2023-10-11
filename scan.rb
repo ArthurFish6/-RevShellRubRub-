d8e2ea6167adbe73d64fe4c0f4ca8d75ca78e61a
ArthurFish6 <124437435+ArthurFish6@users.noreply.github.com>
2023-10-11 20:40:22 +0200

    Add files via upload

---
+++ scan.rb
@@ -0,0 +1,21 @@
+require 'socket'
+
+
+PORT = 4444
+server = TCPServer.new(PORT)
+puts "[*] Server listening on port #{PORT}"
+
+
+loop do
+  client = server.accept
+  puts "[*] Client connected from #{client.peeraddr[2]}:#{client.peeraddr[1]}"
+  while true
+    puts "RETURN:\n#{client.recv(1024)}"
+    cmd = gets.chomp
+    if cmd.empty?
+      cmd = "echo 'Hello World!'\n" 
+    end
+    client.puts cmd
+  end
+  client.close
+end
