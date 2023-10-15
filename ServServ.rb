47c0154f905fe48c36899c88d46099b88f1100ba
ArthurFish6 <124437435+ArthurFish6@users.noreply.github.com>
2023-10-16 00:18:20 +0200

    couleur

--- ServServ.rb
+++ ServServ.rb
@@ -2,12 +2,14 @@ require 'socket'
 
 PORT = 80
 server = TCPServer.new(PORT)
-puts "[*] Server listening on port #{PORT}"
-
+puts "[\033[1;92m*\033[1;97m] Server listening on port #{PORT}"
 
 loop do
   client = server.accept
-  puts "[*] Client connected from #{client.peeraddr[2]}:#{client.peeraddr[1]}"
+  client_address = client.peeraddr[2]
+  client_port = client.peeraddr[1]
+  puts "[\033[1;92m*\033[1;97m] Client connected from #{client_address}:\033[1;94m#{client_port}\033[1;97m"
+  
   while true
     puts "RETURN:\n#{client.recv(1024)}"
     cmd = gets.chomp
